77146ea744b1826c3228fa155e2feef24b659d18
LizzardMedeiros <luis.medeiros@outlook.com.br>
2020-07-24 17:24:19 -0300

    fix app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-/* eslint-disable linebreak-style */
 import React from 'react';
+import { getCategories } from './services/api';
 import './App.css';
 
 function App() {
+  getCategories().then((res) => {
+    console.log(res);
+  })
   return <div className="App">CODECLIMATE</div>;
 }
 
