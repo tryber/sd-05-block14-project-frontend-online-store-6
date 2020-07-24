6723543f005f03e64c1ec5540360ab3be63b527a
Alexander Mitchell <alexlm.trybe@gmail.com>
2020-07-24 17:00:14 -0300

    vai com calma CC

--- src/App.js
+++ src/App.js
@@ -1,30 +1,9 @@
 /* eslint-disable linebreak-style */
-import React from "react";
-import logo from "./logo.svg";
-import { getCategories, getProductsFromCategoryAndQuery } from "./services/api";
-
-import "./App.css";
+import React from 'react';
+import './App.css';
 
 function App() {
-  getProductsFromCategoryAndQuery('MLB1430');
-  return (
-    <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
