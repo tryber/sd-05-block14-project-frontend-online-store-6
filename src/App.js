fe6fea9f59c1228fbb29e600cad14c0ba0b26c4a
Alexander Mitchell <alexlm.trybe@gmail.com>
2020-07-25 03:34:51 -0300

    ProductList

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 /* eslint-disable linebreak-style */
 import React from 'react';
 import './App.css';
+import ProductList from './ProductList'
 
 function App() {
-  return <div className="App">CODECLIMATE</div>;
+  return <div className="App"></div>;
 }
 
 export default App;
