a0f079a2bad67225db8642c0c25b1b7993f7c810
Alexander Mitchell <alexlm.trybe@gmail.com>
2020-07-25 05:10:53 -0300

    primeira tentativa de ex2, delete shop list pois estava na branch errada

--- src/ProductList.js
+++ src/ProductList.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import './img/shopping-cart.svg'
+import ShoppingCart from './components/ShoppingCart';
 
 class ProductList extends React.Component {
   render() {
-    return (
-      <div  data-testid="home-initial-message">
+    return(
+      <div data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
-        <ul>
-        </ul>
       </div>
-    );
+      <div>
+        <button data-testid = 'shopping-cart-button'>
+          <img src={shopping-cart} alt='shopping cart'/>
+        </button>
+      </div>
+      );
   }
 }
 
