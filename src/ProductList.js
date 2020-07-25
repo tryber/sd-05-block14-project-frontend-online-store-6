7ba0ec30d833b5091532be602075cd501b4e2326
Alexander Mitchell <alexlm.trybe@gmail.com>
2020-07-25 05:22:42 -0300

    correção agora sim tem apenas o productlist

--- src/ProductList.js
+++ src/ProductList.js
@@ -1,19 +1,13 @@
 import React from 'react';
-import './img/shopping-cart.svg'
-import ShoppingCart from './components/ShoppingCart';
 
 class ProductList extends React.Component {
   render() {
-    return(
+    return (
+      //implementando espaço de busca
       <div data-testid="home-initial-message">
-          Digite algum termo de pesquisa ou escolha uma categoria.
+        Digite algum termo de pesquisa ou escolha uma categoria.
       </div>
-      <div>
-        <button data-testid = 'shopping-cart-button'>
-          <img src={shopping-cart} alt='shopping cart'/>
-        </button>
-      </div>
-      );
+    );
   }
 }
 
