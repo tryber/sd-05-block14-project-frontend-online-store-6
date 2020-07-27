16756389e849f5fcd185454724dd588c821ac693
LizzardMedeiros <luis.medeiros@outlook.com.br>
2020-07-27 14:31:26 -0300

    All fixed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import React from 'react';
-import { getCategories } from './services/api';
 import './App.css';
 
 function App() {
-  getCategories().then((res) => {
-    console.log(res);
-  })
   return <div className="App">CODECLIMATE</div>;
 }
 
