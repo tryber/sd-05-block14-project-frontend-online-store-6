91656fbe282a625a11f7517e2efe35eaa543ca9c
Alexander Mitchell <alexlm.trybe@gmail.com>
2020-07-28 09:59:27 -0300

    CC tá de zuação né? corrigi 2 apareceu 5 kkk

--- src/components/SearchBar.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import * as Api from '../services/api';
-
-class SearchBar extends React.Component {
-  constructor() {
-    super();
-    this.state = { searchTerm: '', searchResult: [] };
-  }
-}
