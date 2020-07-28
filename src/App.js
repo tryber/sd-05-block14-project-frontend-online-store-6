e2c12fbf718c9d33bc8cbd7c926fa38264950bcc
LizzardMedeiros <luis.medeiros@outlook.com.br>
2020-07-28 16:28:16 -0300

    CC Fixed

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Categories from './components/Categories';
 import ShoppingCart from './pages/ShoppingCart';
 import svg from './img/shopping-cart.svg';
 
+// force CC
+
 function App() {
   return (
     <div className="app">
