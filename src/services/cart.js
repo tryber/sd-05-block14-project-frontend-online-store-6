3340b5e193e938f0c8d8c2890574d5f38cb90787
LizzardMedeiros <luis.medeiros@outlook.com.br>
2020-07-30 20:15:10 -0300

    adicionando funcionalidades

--- src/services/cart.js
+++ src/services/cart.js
@@ -1,32 +1,49 @@
-const cartSelection = {
+function saveStorage(meuDepositorio) {
+  localStorage.setItem('depositório', JSON.stringify(meuDepositorio));
+}
+
+function loadStorage() {
+  return JSON.parse(localStorage.getItem('depositório') || '[]');
+}
+
+export default {
   create: () => {
-    const cart = JSON.parse(localStorage.getItem('depositório') || '[]');
-    if (cart.length === 0) {
-      localStorage.setItem('depositório', '[]');
-    }
+    if (loadStorage() === 0) saveStorage([]);
   },
-  getItems: () => (
-    JSON.parse(localStorage.getItem('depositório') || '[]')
-  ),
+  getItems: () => loadStorage(),
   addItem: (product) => {
+    const meuDepositorio = loadStorage();
     const item = {
       id: product.id,
+      unique: `${product.id}-${Math.floor(Math.random() * 1E7)}`,
       title: product.title,
       price: product.price,
       thumbnail: product.thumbnail,
+      amount: 1,
     };
-    const meuDepositorio = JSON.parse(localStorage.getItem('depositório') || '[]');
-    meuDepositorio.push(item);
-    localStorage.setItem('depositório', JSON.stringify(meuDepositorio));
+    let exists = false;
+    meuDepositorio
+      .forEach((element) => {
+        exists = (item.id === element.id);
+        if (exists) element.amount += 1;
+      });
+    if (!exists) meuDepositorio.push(item);
+    saveStorage(meuDepositorio);
   },
-  removeItem: (productId) => {
-    const meuDepositorio = JSON.parse(localStorage.getItem('depositório'));
-    const newDepositorio = meuDepositorio.filter(({ id }) => (id !== productId));
-    localStorage.setItem('depositório', JSON.stringify(newDepositorio));
+  removeItem: (unique, amount = 1) => {
+    const meuDepositorio = loadStorage();
+    meuDepositorio
+      .forEach((element) => {
+        if (unique === element.unique && element.amount > 0) element.amount -= amount;
+      });
+    saveStorage(meuDepositorio);
+  },
+  deleteItem: (unique) => {
+    const meuDepositorio = loadStorage();
+    const newDepositorio = meuDepositorio.filter(({ unique: un }) => (un !== unique));
+    saveStorage(newDepositorio);
   },
   removeAll: () => {
-    localStorage.setItem('depositório', '[]');
+    saveStorage([]);
   },
 };
-
-export default cartSelection;
